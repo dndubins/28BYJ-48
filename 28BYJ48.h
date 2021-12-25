3e5c50e77caecefb2d3eabeb39e13d0ac9f8df9d
David Dubins <d.dubins@utoronto.ca>
2021-12-25 17:51:38 -0500

    Create 28BYJ48.h

---
+++ 28BYJ48.h
@@ -0,0 +1,19 @@
+/* 28BYJ-48.h
+ * Controlling Stepper with ULN2003
+ * David Dubins 15-Dec-20
+ * Last Updated: 25-Dec-21
+ *
+ */
+
+#ifndef 28BYJ48_h
+#define 28BYJ48_h
+
+class StepperMotor {
+public:
+    StepperMotor(byte in1, byte in2, byte in3, byte in4);    // Constructor to set in1-4 pins
+    float rpm; // to hold rpm  
+    void step(int n, float speed);         // Step a certain number of steps. + for one way and - for the other
+    byte IN[4];      // to hold digital pin numbers for control wires
+};
+
+#endif
